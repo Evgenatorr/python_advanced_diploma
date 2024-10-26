9ee47d7076a3573fda2689022f8998d0301f029c
evgenator <zenyavolobuev@mail.ru>
2024-10-26 15:33:58 +0700

    незначительные исправления и добавления тестов

--- src/tests/test_tweet.py
+++ src/tests/test_tweet.py
@@ -19,6 +19,15 @@ async def test_create_tweet(async_client: AsyncClient):
     assert response.json()['tweet_id'] == 2
 
 
+async def test_get_tweets(async_client: AsyncClient):
+    response = await async_client.get(
+        "/api/tweets",
+        headers={"api-key": "test"}
+    )
+    assert response.status_code == 200
+    assert len(response.json()['tweets']) == 2
+
+
 async def test_create_tweet_invalid_json(async_client: AsyncClient):
     response = await async_client.post(
         "/api/tweets", json={"title": "something"},
@@ -33,3 +42,20 @@ async def test_create_tweet_invalid_header(async_client: AsyncClient):
         headers={"api-key": "test3"}
     )
     assert response.status_code == 401  # Unauthorized
+
+
+async def test_delete_tweet(async_client: AsyncClient):
+    tweet_id = 1
+    response = await async_client.delete(
+        f"/api/tweets/{tweet_id}", headers={"api-key": "test"},
+    )
+    assert response.status_code == 200
+
+    # проверяем что твит удалился и из двух остался один твит
+    response = await async_client.get(
+        "/api/tweets",
+        headers={"api-key": "test"}
+    )
+    assert response.status_code == 200
+    assert len(response.json()['tweets']) == 1
+
